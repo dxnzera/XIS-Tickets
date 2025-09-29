576903b016fb6ddfa13294dc67f9beb49df9e61c
Daniel Oliveira <117216540+dxnzera@users.noreply.github.com>
2025-09-29 17:58:11 -0300

    Initial Commit

---
+++ XIS Tickets/XIS_TicketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XIS_TicketsApp.swift
+//  XIS Tickets
+//
+//  Created by Daniel Oliveira Mendes on 29/09/25.
+//
+
+import SwiftUI
+
+@main
+struct XIS_TicketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
